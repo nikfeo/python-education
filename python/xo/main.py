dc523c6b149d7f3696fc2cca89ea497b0b130ca6
nikfeo <nickfeoktistov@gmail.com>
2021-05-23 14:04:57 +0300

    Added player input function with validation and corrected some mistakes

--- python/xo/main.py
+++ python/xo/main.py
@@ -1,28 +1,30 @@
 """Main module. Console game Tic Tac Toe"""
 
-player_1 = ''
-player_2 = ''
+
+PLAYER_1 = ''
+PLAYER_2 = ''
+field_cells = list(range(10))
 
 
 def draw_field(field):
-    """There will be a docstring"""
+    """Here will be a docstring"""
     print("-------------")
     for i in range(3):
-        print("|", field[i*3 + 1], "|",
-              field[i*3 + 2], "|",
-              field[i*3 + 3], "|")
+        print("|", field[i * 3 + 1], "|",
+              field[i * 3 + 2], "|",
+              field[i * 3 + 3], "|")
         print("-------------")
 
 
 def input_player_names():
-    """There will be a docstring"""
-    global player_1, player_2
-    player_1 = input("Please, enter name first user: ")
-    player_2 = input("Please, enter name second user: ")
+    """Here will be a docstring"""
+    global PLAYER_1, PLAYER_2
+    PLAYER_1 = input("Please, enter name first user: ")
+    PLAYER_2 = input("Please, enter name second user: ")
 
 
 def show_menu():
-    """There will be a docstring"""
+    """Here will be a docstring"""
     menu_choice = 0
     while menu_choice == 0:
 
@@ -46,59 +48,68 @@ def show_menu():
             print("Please enter number from menu")
 
 
-def check_win(field_cells):
-    """There will be a docstring"""
-    win_sets = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+def check_win(field):
+    """Here will be a docstring"""
+    win_sets = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
+                (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
     for i in win_sets:
-        if field_cells[i[0]] == field_cells[i[1]] == field_cells[i[2]] == 'X':
-            return player_1
-        elif field_cells[i[0]] == field_cells[i[1]] == field_cells[i[2]] == '0':
-            return player_2
+        if field[i[0]] == field[i[1]] == field[i[2]] == 'X':
+            return PLAYER_1
+        if field[i[0]] == field[i[1]] == field[i[2]] == '0':
+            return PLAYER_2
     return False
 
 
+def take_player_input(player_symbol, player_name):
+    """Here will be a docstring"""
+    valid = False
+    while not valid:
+        player_input = input(f"{player_name} your turn! Enter number of free cell: ")
+        try:
+            player_input = int(player_input)
+        except ValueError:
+            print("Enter a number from 1 to 9")
+            continue
+        if player_input in range(1, 10):
+            if str(field_cells[player_input]) not in "X0":
+                field_cells[player_input] = player_symbol
+                valid = True
+            else:
+                print("This cell is already taken")
+        else:
+            print("Enter a number from 1 to 9")
+
+
 def play_game():
-    """There will be a docstring"""
-    print(f"Player 1: {player_1} plays with 'X'\n"
-          f"Player 2: {player_2} plays with '0'")
-    field_cells = list(range(10))
+    """Here will be a docstring"""
+    print(f"Player 1: {PLAYER_1} plays with 'X'\n"
+          f"Player 2: {PLAYER_2} plays with '0'")
     draw_field(field_cells)
     counter = 0
-
-    while True:
+    win = False
+    while not win:
         winner_player = check_win(field_cells)
         if winner_player:
+            # не понимаю, как поменять перменную win, чтобы прервать цикл?
+            win = True
             print(f"Winner is {winner_player}")
             break
         if counter == 9:
             print("The game is a draw! Nobody won")
             break
-        # нужно написать отдельную функцию для проверки ввода числа пользователем
         if not counter % 2:
-            player1_turn = input(f"{player_1} your turn! Enter number of free cell: ")
-            try:
-                player1_turn = int(player1_turn)
-                field_cells[player1_turn] = "X"
-            except ValueError:
-                print("enter a number from 1 to 9")
-                continue
+            take_player_input('X', PLAYER_1)
         else:
-            player2_turn = input(f"{player_2} your turn! Enter number of free cell: ")
-            try:
-                player2_turn = int(player2_turn)
-                field_cells[player2_turn] = "0"
-            except ValueError:
-                print("enter a number from 1 to 9")
-                continue
+            take_player_input('0', PLAYER_2)
         counter += 1
         draw_field(field_cells)
 
     # нужно еще додумать с рестартом игры, сейчас выбор не влияет на рестарт (из-за цикла While?)
-    restart = input('Do yot want to play again? Enter y/n: ')
-    if restart == 'y' or 'Y':
-        play_game()
-    elif restart == 'n':
-        show_menu()
+    # restart = input('Do yot want to play again? Enter y/n: ')
+    # if restart == 'y' or 'Y':
+    #     play_game()
+    # elif restart == 'n':
+    #     show_menu()
 
 
 if __name__ == "__main__":

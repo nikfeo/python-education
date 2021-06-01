5e0bebb390b96f22d56c28b8eaf8e744a0748b5a
nikfeo <nickfeoktistov@gmail.com>
2021-06-01 22:56:47 +0300

    Added 'tail' to linked_list.py and fixed some bugs

--- algorithms_data/07_data_structures/linked_list.py
+++ algorithms_data/07_data_structures/linked_list.py
@@ -1,12 +1,19 @@
-"""This module describes singly linked list"""
+"""
+This module describes singly linked list
+"""
 
 
 class LinkedList:
-    """Describes singly linked list"""
+    """
+    Describes singly linked list
+    """
     head = None
+    tail = None
 
     class Node:
-        """Describes Node class with data and next_item info"""
+        """
+        Describes Node class with data and next_item info
+        """
         data = None
         next_item = None
 
@@ -15,7 +22,9 @@ class LinkedList:
             self.next_item = next_item
 
     def __len__(self):
-        """Calculates length of the list and returns integer"""
+        """
+        Calculates length of the list and returns integer
+        """
         if not self.head:
             return 0
         length = 1
@@ -26,18 +35,33 @@ class LinkedList:
         return length
 
     def append(self, data):
-        """Adds new node with data to the end of list"""
+        """
+        Adds new node with data to the end of list
+        """
+        node = self.Node(data)
         if not self.head:
-            self.head = self.Node(data)
-            return data
-        node = self.head
-        while node.next_item:
-            node = node.next_item
-        node.next_item = self.Node(data)
-        return data
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next_item = node
+            self.tail = node
+
+    def prepend(self, data):
+        """
+        Adds new node with data to the beginning of list
+        """
+        node = self.Node(data)
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            self.head.next_item = node
+            self.head = node
 
     def display(self):
-        """Displays each item of the list on new string"""
+        """
+        Displays each item of the list on new string
+        """
         node = self.head
         while node.next_item:
             print(node.item)
@@ -45,7 +69,9 @@ class LinkedList:
         print(node.item)
 
     def insert(self, item, index):
-        """Inserts item in list, using index"""
+        """
+        Inserts item in list, using index
+        """
         position = 0
         node = self.head
         previous_node = self.head
@@ -57,16 +83,23 @@ class LinkedList:
         return item
 
     def get(self, index):
-        """Allows to lookup item by index"""
+        """
+        Allows to get item by index
+        """
         position = 0
         node = self.head
+        if index == len(self) - 1:
+            node = self.tail
+            return node.item
         while position < index:
             node = node.next_item
             position += 1
         return node.item
 
     def delete(self, index):
-        """Deletes item from list by index"""
+        """
+        Deletes item from list by index
+        """
         if index == 0:
             self.head = self.head.next_item
         node = self.head
@@ -95,3 +128,7 @@ if __name__ == '__main__':
 
     ll.delete(0)
     ll.display()
+
+    print('\n', ll.get(0))
+    print(ll.get(1))
+    print(ll.get(2))

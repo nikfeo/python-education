00ec549fa9a7b579edb8cdbfa96637fba331ad5d
nikfeo <nickfeoktistov@gmail.com>
2021-06-15 10:03:10 +0300

    Created directory 2-3_practice

---
+++ database/2-3_practice/task_3-1.sql
@@ -0,0 +1,7 @@
+/* Create a new table potential customers
+   with fields id, email, name, surname, second_name, city
+   Fill in the data in the table.
+   Print names and email of potential and existing users from city 17
+   */
+
+
